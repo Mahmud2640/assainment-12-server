5d019787d5fd4af52629dca408d48b8d98fd6b98
Mahmud2640 <mdmahmud484@gmail.com>
2022-05-25 01:24:08 +0600

    mongo connect

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World! this is a test')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjpc8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("h-power").collection("services");
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello World! this is a test");
+});
 
 app.listen(port, () => {
-  console.log(`manufacture app listening on port ${port}`)
-})
+  console.log(`manufacture app listening on port ${port}`);
+});
